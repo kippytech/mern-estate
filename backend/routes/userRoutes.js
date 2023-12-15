a57aa9f9b55d41f3aab83ce6d227dc517e003a4e
kippytech <dkipyegon.dk7.dk@gmail.com>
2023-12-15 15:34:12 +0300

    add complete listing page, contact component and user api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getUsers, updateUser, deleteUser, getUserListings } = require('../controllers/userController')
+const { getUsers, updateUser, deleteUser, getUserListings, getUser } = require('../controllers/userController')
 const verifyToken = require('../utils/verifyUser')
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.get('/users', getUsers)
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 module.exports = router
