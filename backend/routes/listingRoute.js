3e1f3f4bbd5b666195cb26f0e22ddbc0751104b4
kippytech <dkipyegon.dk7.dk@gmail.com>
2023-12-16 13:42:46 +0300

    create search api route

--- backend/routes/listingRoute.js
+++ backend/routes/listingRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createListing, deleteListing, updateListing, getListing } = require('../controllers/listingController')
+const { createListing, deleteListing, updateListing, getListing, getListings } = require('../controllers/listingController')
 const verifyToken = require('../utils/verifyUser')
 
 const router = express.Router()
@@ -8,5 +8,6 @@ router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updateListing)
 router.get('/get/:id', getListing)
+router.get('/get', getListings)
 
 module.exports = router
