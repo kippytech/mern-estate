3f6f4a6340d9be14002248512ab2d0d0d2fccc04
kippytech <dkipyegon.dk7.dk@gmail.com>
2023-12-17 13:13:31 +0300

    complete about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='px-4 py-20 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Maskani Estate</h1>
+      <p className='mb-4 text-slate-700'>Maskani Estate is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighbourhoods. Our team of experienced agents us dedicated to providing exceptional services and making the buying and selling process as seamless as possible.</p>
+      <p className='mb-4 text-slate-700'>Our mission is to help our clients achieve their real estate goals by proving expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.</p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
   )
 }
